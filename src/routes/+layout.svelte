e37240816c18955a4a634829d1dce25246eeb88f
NellyKaykay <elelyonexclusive@gmail.com>
2026-01-06 21:32:29 +0100

    fix: use <html lang='es'></html> in svelte:head for Svelte compliance

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,12 +10,10 @@
 <svelte:head>
   <title>TicketsDMK | Entradas para conciertos y eventos</title>
 
-
   <meta
     name="description"
     content="Compra entradas para conciertos y eventos en Barcelona y otras ciudades. TicketsDMK ofrece venta segura, rápida y sin complicaciones."
   />
-  <meta name="msvalidate.01" content="ED80D69059F9688D92F8E33855897834" />
 
   <link rel="canonical" href="https://ticketsdmk.com/" />
 
